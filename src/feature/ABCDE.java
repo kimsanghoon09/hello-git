4304025b09ebab3c90a6d615dd3a30c97aab32b1
kimsanghoon09 <realgay09@gmail.com>
2023-05-16 17:11:42 +0900

    Update ABCDE.java

--- src/feature/ABCDE.java
+++ src/feature/ABCDE.java
@@ -1,10 +1,6 @@
 package feature;
 
-<<<<<<< HEAD
-// 이것은 master에서 쓴 ABCDE 주석입니다.
-=======
-// 이건 feature에서 쓴 ABCDE 주석입니다.
->>>>>>> feature
+// 이것은 master에서 쓴 주석입니다.
 public class ABCDE {
 	private void abc() {
 		System.out.println("#feature가 최고야!");
